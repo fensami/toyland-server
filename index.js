7be157416eaca24ab25dafa16543b04f208374f6
fensami <113832498+fensami@users.noreply.github.com>
2023-05-19 11:34:01 +0600

    connet to mongodb database and added some data

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+console.log(process.env.DB_USERNAME);
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.fs0mclr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const allToysCollection = client.db('toyLand').collection('allToys')
+
+
+    app.post('/allToys', async(req, res) => {
+        const addToys = req.body;
+        console.log(addToys);
+        const result =await allToysCollection.insertOne(addToys);
+        res.send(result)
+    })
+
+    // app.post('/bookings', async(req, res) => {
+    //     const booking =req.body;
+    //     console.log(booking);
+    //     const result = await bookingCollection.insertOne(booking);
+    //     res.send(result)
+    //   })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
